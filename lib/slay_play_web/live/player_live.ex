09d2f14c82561bafac58aa439c20b261cfa6fe11
Tom Grozev <1491414+TomGrozev@users.noreply.github.com>
2022-12-04 15:11:32 +1100

    Add toggle for player controls

--- lib/slay_play_web/live/player_live.ex
+++ lib/slay_play_web/live/player_live.ex
@@ -12,112 +12,117 @@ defmodule SlayPlayWeb.PlayerLive do
       <div id="audio-ignore" phx-update="ignore">
         <audio></audio>
       </div>
-      <div class="bg-white dark:bg-gray-800 p-4">
-        <div class="flex items-center space-x-3.5 sm:space-x-5 lg:space-x-3.5 xl:space-x-5">
-          <div class="pr-5">
-            <div class="min-w-0 max-w-xs flex-col space-y-0.5">
-              <h2 class="text-black dark:text-white text-sm sm:text-sm lg:text-sm xl:text-sm font-semibold truncate">
-                <%= if @song, do: @song.title, else: raw("&nbsp;") %>
-              </h2>
-              <p class="text-gray-500 dark:text-gray-400 text-sm sm:text-sm lg:text-sm xl:text-sm font-medium">
-                <%= if @song, do: @song.artist, else: raw("&nbsp;") %>
-              </p>
+      <button phx-click={toggle_player()} class="absolute -mt-6 px-1 bg-gray-200 rounded-tl-lg rounded-tr-lg right-4">
+        <.icon name={:chevron_up} />
+      </button>
+      <div id="audio-controls" class="relative hidden">
+        <div class="bg-white dark:bg-gray-800 p-4">
+          <div class="flex items-center space-x-3.5 sm:space-x-5 lg:space-x-3.5 xl:space-x-5">
+            <div class="pr-5">
+              <div class="min-w-0 max-w-xs flex-col space-y-0.5">
+                <h2 class="text-black dark:text-white text-sm sm:text-sm lg:text-sm xl:text-sm font-semibold truncate">
+                  <%= if @song, do: @song.title, else: raw("&nbsp;") %>
+                </h2>
+                <p class="text-gray-500 dark:text-gray-400 text-sm sm:text-sm lg:text-sm xl:text-sm font-medium">
+                  <%= if @song, do: @song.artist, else: raw("&nbsp;") %>
+                </p>
+              </div>
+            </div>
+            <.progress_bar id="player-progress" />
+            <div
+              id="player-info"
+              class="text-gray-500 dark:text-gray-400 flex-row justify-between text-sm font-medium tabular-nums"
+              phx-update="ignore"
+            >
+              <div id="player-time"></div>
+              <div id="player-duration"></div>
             </div>
           </div>
-          <.progress_bar id="player-progress" />
-          <div
-            id="player-info"
-            class="text-gray-500 dark:text-gray-400 flex-row justify-between text-sm font-medium tabular-nums"
-            phx-update="ignore"
-          >
-            <div id="player-time"></div>
-            <div id="player-duration"></div>
-          </div>
         </div>
-      </div>
-      <div class="bg-gray-50 text-black dark:bg-gray-900 dark:text-white px-1 sm:px-3 lg:px-1 xl:px-3 grid grid-cols-3 items-center">
-        <!-- prev -->
-        <button
-          type="button"
-          class="sm:block xl:block mx-auto scale-75"
-          phx-click={js_prev()}
-          aria-label="Previous"
-        >
-          <svg width="17" height="18">
-            <path d="M0 0h2v18H0V0zM4 9l13-9v18L4 9z" fill="currentColor" />
-          </svg>
-        </button>
-        <!-- /prev -->
-        <!-- play/pause -->
-        <button
-          type="button"
-          class="mx-auto scale-75"
-          phx-click={js_play_pause()}
-          aria-label={
-            if @playing do
-              "Pause"
-            else
-              "Play"
-            end
-          }
-        >
-          <%= if @playing do %>
-            <svg id="player-pause" width="50" height="50" fill="none">
-              <circle
-                class="text-gray-300 dark:text-gray-500"
-                cx="25"
-                cy="25"
-                r="24"
-                stroke="currentColor"
-                stroke-width="1.5"
-              />
-              <path d="M18 16h4v18h-4V16zM28 16h4v18h-4z" fill="currentColor" />
+        <div class="bg-gray-50 text-black dark:bg-gray-900 dark:text-white px-1 sm:px-3 lg:px-1 xl:px-3 grid grid-cols-3 items-center">
+          <!-- prev -->
+          <button
+            type="button"
+            class="sm:block xl:block mx-auto scale-75"
+            phx-click={js_prev()}
+            aria-label="Previous"
+          >
+            <svg width="17" height="18">
+              <path d="M0 0h2v18H0V0zM4 9l13-9v18L4 9z" fill="currentColor" />
             </svg>
-          <% else %>
-            <svg
-              id="player-play"
-              width="50"
-              height="50"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <circle
-                id="svg_1"
-                stroke-width="0.8"
-                stroke="currentColor"
-                r="11.4"
-                cy="12"
-                cx="12"
-                class="text-gray-300 dark:text-gray-500"
-              />
-              <path
-                stroke="null"
-                fill="currentColor"
-                transform="rotate(90 12.8947 12.3097)"
-                id="svg_6"
-                d="m9.40275,15.10014l3.49194,-5.58088l3.49197,5.58088l-6.98391,0z"
-                stroke-width="1.5"
+          </button>
+          <!-- /prev -->
+          <!-- play/pause -->
+          <button
+            type="button"
+            class="mx-auto scale-75"
+            phx-click={js_play_pause()}
+            aria-label={
+              if @playing do
+                "Pause"
+              else
+                "Play"
+              end
+            }
+          >
+            <%= if @playing do %>
+              <svg id="player-pause" width="50" height="50" fill="none">
+                <circle
+                  class="text-gray-300 dark:text-gray-500"
+                  cx="25"
+                  cy="25"
+                  r="24"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                />
+                <path d="M18 16h4v18h-4V16zM28 16h4v18h-4z" fill="currentColor" />
+              </svg>
+            <% else %>
+              <svg
+                id="player-play"
+                width="50"
+                height="50"
+                xmlns="http://www.w3.org/2000/svg"
                 fill="none"
-              />
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <circle
+                  id="svg_1"
+                  stroke-width="0.8"
+                  stroke="currentColor"
+                  r="11.4"
+                  cy="12"
+                  cx="12"
+                  class="text-gray-300 dark:text-gray-500"
+                />
+                <path
+                  stroke="null"
+                  fill="currentColor"
+                  transform="rotate(90 12.8947 12.3097)"
+                  id="svg_6"
+                  d="m9.40275,15.10014l3.49194,-5.58088l3.49197,5.58088l-6.98391,0z"
+                  stroke-width="1.5"
+                  fill="none"
+                />
+              </svg>
+            <% end %>
+          </button>
+          <!-- /play/pause -->
+          <!-- next -->
+          <button
+            type="button"
+            class="mx-auto scale-75"
+            phx-click={js_next()}
+            aria-label="Next"
+          >
+            <svg width="17" height="18" viewBox="0 0 17 18" fill="none">
+              <path d="M17 0H15V18H17V0Z" fill="currentColor" />
+              <path d="M13 9L0 0V18L13 9Z" fill="currentColor" />
             </svg>
-          <% end %>
-        </button>
-        <!-- /play/pause -->
-        <!-- next -->
-        <button
-          type="button"
-          class="mx-auto scale-75"
-          phx-click={js_next()}
-          aria-label="Next"
-        >
-          <svg width="17" height="18" viewBox="0 0 17 18" fill="none">
-            <path d="M17 0H15V18H17V0Z" fill="currentColor" />
-            <path d="M13 9L0 0V18L13 9Z" fill="currentColor" />
-          </svg>
-        </button>
-        <!-- next -->
+          </button>
+          <!-- next -->
+        </div>
       </div>
       <.modal
         id="enable-audio"
@@ -286,5 +291,13 @@ defmodule SlayPlayWeb.PlayerLive do
 
   defp js_listen_now(js \\ %JS{}) do
     JS.dispatch(js, "js:listen_now", to: "#audio-player")
+  end
+
+  defp toggle_player do
+    JS.toggle(
+      to: "#audio-controls",
+      in: {"ease-out duration-300", "translate-y-full", "translate-y-0"},
+      out: {"ease-in duration-200", "translate-y-0", "translate-y-full"}
+    )
   end
 end
